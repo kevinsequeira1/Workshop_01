2d38e850940ae059ff5d9f3177beade7f39fdb41
unknown <kevinsequeira1>
2020-01-20 21:02:30 -0600

    ggg

---
+++ index.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<?php
+  // Obteniendo la fecha actual con hora, minutos y segundos en PHP
+  $fechaActual = date('d-m-Y H:i:s');
+   
+  echo $fechaActual
+?>
+</body>
+</html>
